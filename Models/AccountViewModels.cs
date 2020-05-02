b3c51df8287d4ee3f685051f15c2ee520b62749c
sdayneko <sdaineko@luxoft.com>
2020-05-02 12:40:53 +0300

    added identity models

---
+++ Models/AccountViewModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class ExternalLoginViewModel
+    {
+        public string Url { get; set; }
+
+        public string State { get; set; }
+    }
+
+    public class ManageInfoViewModel
+    {
+        public string LocalLoginProvider { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<UserLoginInfoViewModel> Logins { get; set; }
+
+        public IEnumerable<ExternalLoginViewModel> ExternalLoginProviders { get; set; }
+    }
+
+    public class UserInfoViewModel
+    {
+        public string Email { get; set; }
+
+        public bool HasRegistered { get; set; }
+
+        public string LoginProvider { get; set; }
+    }
+
+    public class UserLoginInfoViewModel
+    {
+        public string LoginProvider { get; set; }
+
+        public string ProviderKey { get; set; }
+    }
+}
